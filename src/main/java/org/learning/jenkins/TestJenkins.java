917e8291fd45e8f81b40972bd44baf706a174137
Dengbowen <stuartd@qq.com>
2019-03-06 21:09:44 +0800

    test get resume v2.0

--- src/main/java/org/learning/jenkins/TestJenkins.java
+++ src/main/java/org/learning/jenkins/TestJenkins.java
@@ -1,10 +1,14 @@
 package org.learning.jenkins;
 
-import org.learning.jenkins.util.QiniuUtil;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.UnsupportedEncodingException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 
 @RestController
 public class TestJenkins {
@@ -15,8 +19,33 @@ public class TestJenkins {
     }
 
     @GetMapping(value = "/getResume")
-    public String getResumeUrl() throws UnsupportedEncodingException {
-        return QiniuUtil.downloadFile("简历测试tomapplen.pdf");
+    @ResponseBody
+    public void getResumeUrl(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        File file = new ClassPathResource("static/test_resume.pdf").getFile();
+        if (!file.exists()) {
+            return;
+        }
+
+        //下载的文件携带这个名称
+        response.setHeader("Content-Disposition", "attachment;filename=" + file.getName());
+        //二进制文件
+        response.setContentType("application/octet-stream");
+
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            byte[] content = new byte[fis.available()];
+            fis.read(content);
+            fis.close();
+
+            ServletOutputStream sos = response.getOutputStream();
+            sos.write(content);
+
+            sos.flush();
+            sos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
